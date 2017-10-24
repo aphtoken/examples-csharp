1a8fb0765683fe41b18143a0e6901e1bd4ef2dc6
Joel Garcia <joel@mobileaws.com>
2017-10-24 07:20:41 -0700

    creating some test examples for the SimpleContract to test out avm loading in Neo-Gui

---
+++ SimpleContractTest/SimpleContractTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Neo;
+using Neo.VM;
+using Neo.Cryptography;
+
+namespace SimpleContractTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new ExecutionEngine(null, Crypto.Default);
+            engine.LoadScript(File.ReadAllBytes("C:\\src\\Aphtoken\\examples-csharp\\SimpleContract\\SimpleContract\\bin\\Debug\\SimpleContract.avm"));
+
+            using (ScriptBuilder sb = new ScriptBuilder())
+            {
+                sb.EmitPush(2); // corresponds to the parameter c
+                sb.EmitPush(4); // corresponds to the parameter b
+                sb.EmitPush(3); // corresponds to the parameter a
+                engine.LoadScript(sb.ToArray());
+            }
+
+            engine.Execute(); // start execution
+
+            var result = engine.EvaluationStack.Peek().GetBigInteger(); // set the return value here
+            Console.WriteLine($"Execution result {result}");
+            Console.ReadLine();
+        }
+    }
+}
